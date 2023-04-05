6feb99b59a8db6c4caf060958d8c993109d6f8a9
projetosstachowski <100374294+projetosstachowski@users.noreply.github.com>
2023-04-05 14:33:24 -0300

    Add files via upload

---
+++ AULA 05 - Exercício Guiado - Conta Bancária/conta_bancaria/Conta.cs
@@ -0,0 +1,23 @@
+﻿namespace conta_bancaria;
+class Conta
+{
+    public string? nomeCliente {get; set;}
+    public int numeroConta {get; set;}
+    private double saldo {get; set;}
+    public double limite {get; set;}
+
+    //Método para realizar um depósito
+    public void depositar (double valor){
+        this.saldo += valor;
+    }
+
+    //Método para realizar um saque
+    public void sacar (double valor){
+        this.saldo -= valor;
+    }
+
+    //Método ConsultaSaldo - retorna um saldo disponível + limite
+    public double ConsultaSaldo(){
+        return this.saldo + this.limite;
+    }
+}
---
+++ AULA 05 - Exercício Guiado - Conta Bancária/conta_bancaria/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace conta_bancaria;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exercício Conta Bancária - ETECMCM");
+        Conta conta = new Conta();
+        conta.nomeCliente = "Nome do cliente";
+        
+        conta.limite = 500;
+        
+        conta.depositar(200);
+
+        conta.sacar(50);
+
+
+        //Método ConsultaSaldo:
+        double saldo = conta.ConsultaSaldo();
+
+        //Para vizualizar:
+        Console.WriteLine("Seu saldo é de: " + saldo);
+
+
+        //Console.WriteLine("Olá, " + conta.nomeCliente + ", seu saldo é de: " + conta.saldo);
+        //Console.WriteLine("Seu limite de saque é de: " + conta.limite);
+        //Console.WriteLine("O número de sua conta é: " + conta.numeroConta);
+    }
+}
